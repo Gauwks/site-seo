1f1799897b773d8fe63dbfa822fcaaa7b6cfa0b2
Mahwks <maknae.games7@gmail.com>
2024-10-30 15:32:23 -0300

    Upload do site
    Site da sala com o trabalho sobre SEO

---
+++ script.js
@@ -0,0 +1,6 @@
+window.revelar = ScrollReveal ({reset:true });
+
+ScrollReveal().reveal('section.module.parallax h1', {duration: 2000, distance: '40px' })
+
+ScrollReveal().reveal('.container', {duration: 1000, distance: '40px' })
+
